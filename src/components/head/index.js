7d26055ceab820a18ab166ef798d9e6808d43365
John <634408262@qq.com>
2018-12-21 14:52:12 +0800

    修改样式

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './index.scss';
 import { Menu, Icon, Row, Col } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
 
 class Head extends React.Component {
     state = {
@@ -18,31 +16,33 @@ class Head extends React.Component {
 
     render() {
         return (
-            <Row type="flex" justify="center">
-                <Col md={8} xs={24} sm={24} className="hidden-sm-and-down">
-                    <div className="combined-bar"></div>
-                    <div className="logo">
-                        xzzdll Blog
+            <div className="head">
+                <Row type="flex" justify="center">
+                    <Col md={5} xs={24} sm={24} className="hidden-sm-and-down">
+                        <div className="combined-bar"></div>
+                        <div className="logo">
+                            xzzdll Blog
       </div>
-                </Col>
-                <Col className="menu" md={10} xs={24} sm={24}>
-                    <Menu
-                        onClick={this.handleClick}
-                        selectedKeys={[this.state.current]}
-                        mode="horizontal"
-                    >
-                        <Menu.Item key="mail">
-                            <Icon type="mail" />文章
+                    </Col>
+                    <Col className="menu" md={15} xs={24} sm={24}>
+                        <Menu
+                            onClick={this.handleClick}
+                            selectedKeys={[this.state.current]}
+                            mode="horizontal"
+                        >
+                            <Menu.Item key="mail">
+                                <Icon type="mail" />文章
       </Menu.Item>
-                        <Menu.Item key="app">
-                            <Icon type="appstore" />说说
+                            <Menu.Item key="app">
+                                <Icon type="appstore" />说说
       </Menu.Item>
-                        <Menu.Item key="alipay">
-                            <Icon type="alipay" />归档
+                            <Menu.Item key="alipay">
+                                <Icon type="alipay" />归档
       </Menu.Item>
-                    </Menu>
-                </Col>
-            </Row>
+                        </Menu>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
